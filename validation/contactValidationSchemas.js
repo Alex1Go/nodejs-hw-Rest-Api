88fa7ee7b687413dfec5e8770c535877f64f6111
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-11-16 20:50:59 +0200

    add

--- validation/contactValidationSchemas.js
+++ validation/contactValidationSchemas.js
@@ -1,12 +1,12 @@
 const Joi = require("joi");
 
 const schema = Joi.object({
-  name: Joi.string().min(3).max(30),
+  name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email({
     minDomainSegments: 2,
     tlds: { allow: ["com", "net"] },
   }),
-  phone: Joi.string(),
+  phone: Joi.string().required(),
   favorite: Joi.boolean(),
 });
 
